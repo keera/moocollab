0b70bd1d480c9cf0e5891a76003e2c43f4a0e3aa
Alan <linpd2@gmail.com>
2013-11-15 20:16:25 -0500

    hooked up course search

--- server.js
+++ server.js
@@ -1,12 +1,56 @@
 var Sequelize = require("sequelize");
 var settings = require('./settings');
+var express = require('express');
+var app = express();
 
 var sqlz = new Sequelize(settings.dbname, settings.dbuser, settings.dbpass, {
   dialect: 'mysql'
 });
 
-sqlz.query("SELECT * FROM user").success(function(myTableRows) {
-  console.log(myTableRows)
-})
+// Define models
+var course = sqlz.define('course', {
+  course_id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: Sequelize.STRING,
+  provider_id: Sequelize.INTEGER
+}, {
+  freezeTableName: true,
+  timestamps: false
+});
 
-console.log(sqlz);
+var provider = sqlz.define('provider', {
+  provider_id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: Sequelize.STRING
+}, {
+  freezeTableName: true,
+  timestamps: false
+});
+
+// Set association
+course.belongsTo(provider, {foreignKey: 'provider_id'});
+
+// Set the public folder
+app.use(express.static(__dirname + '/client'));
+
+app.get('/', function(req, res) {
+  res.status(200).sendfile('index.html');
+});
+
+// Course search
+app.get('/course/:name', function(req, res) {
+  course.findAll({
+    include: [provider],
+    where: ["course.name LIKE ?", '%' + req.params.name + '%'],
+    limit: 5}).success(function(courses) {
+    res.json(200,courses);
+  });
+});
+
+app.listen(3000);
