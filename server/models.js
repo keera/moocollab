1167dcc93118b5fdd449c4f709a2e6c58a146242
Alan <linpd2@gmail.com>
2013-11-17 16:28:06 -0500

    added group creation

--- server/models.js
+++ server/models.js
@@ -2,7 +2,8 @@ var Sequelize = require("sequelize");
 var settings = require('../settings');
 
 var sqlz = new Sequelize(settings.dbname, settings.dbuser, settings.dbpass, {
-  dialect: 'mysql'
+  dialect: 'mysql',
+  omitNull: true
 });
 
 // Define models
@@ -56,10 +57,44 @@ var User = sqlz.define('user', {
   timestamps: false
 });
 
-// Set association
+var Group = sqlz.define('group', {
+  group_id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: Sequelize.STRING,
+  create_date: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW
+  },
+  course_id: Sequelize.INTEGER
+}, {
+  freezeTableName: true,
+  timestamps: false
+});
+
+var GroupUser = sqlz.define('group_user', {
+  group_id: Sequelize.INTEGER,
+  user_id: Sequelize.INTEGER,
+  user_join_date: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW
+  },
+  user_confirm_send_date: Sequelize.DATE,
+  user_confirm_date: Sequelize.DATE
+}, {
+  freezeTableName: true,
+  timestamps: false
+});
+
+GroupUser.belongsTo(Group, {foreignKey: 'group_id'});
 Course.belongsTo(Provider, {foreignKey: 'provider_id'});
 
 module.exports.User = User;
 module.exports.Course = Course;
 module.exports.Provider = Provider;
+module.exports.Group = Group;
+module.exports.GroupUser = GroupUser;
+module.exports.Sqlz = sqlz;
 
